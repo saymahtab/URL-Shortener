72a0b7e6f9261bbe8d5bee76d161d12c6b4bae62
saymahtab <reachmahtabsce@gmail.com>
2024-08-19 18:13:10 +0530

    update

--- public/JS/script.js
+++ public/JS/script.js
@@ -2,7 +2,7 @@ document.getElementById('copyButton').addEventListener('click', function() {
     const shortUrl = document.querySelector('.sh-url');
     const resultContainer = document.querySelector('.result');
     
-    navigator.clipboard.writeText(`flink-n946.onrender.com//${shortUrl.innerText.split('/')[1]}`)
+    navigator.clipboard.writeText(`flink-n946.onrender.com/${shortUrl.innerText.split('/')[1]}`)
     .then(() => {
         const tooltip = document.createElement('span');
         tooltip.className = 'tooltip';
